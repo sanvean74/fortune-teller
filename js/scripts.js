f13a447d3cfb88c9dc70e788d77e05a844145fa9
Donna Lambert <dlambertpdx@gmail.com>
2017-08-24 12:06:10 -0700

    fix button type in html add list element to js

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
     $("#unlucky_response").show();
     $("input:checkbox[name=unlucky]:checked").each(function() {
       var unluckyResponse = $(this).val();
-      $("#unlucky_response").append(unluckyResponse + "<br>");
+      $("#unlucky_response").append("<li>" + unluckyResponse + "</li>");
     });
     $("#lucky_response").show();
     $("input:checkbox[name=lucky]:checked").each(function() {
       var luckyResponse = $(this).val();
-      $("#lucky_response").append(luckyResponse + "<br>");
+      $("#lucky_response").append("<li>" + luckyResponse + "</li>");
     });
     $("#fortune_teller").hide();
   });
